cfd6244a6b8038c75e2dd4227afd1f8e57ca710a
geoffreyjiang <geoffreychjiang@gmail.com>
2023-07-30 18:17:15 -0700

    longest seq

---
+++ arrays&hashing/longestSequence.js
@@ -0,0 +1,32 @@
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+// You must write an algorithm that runs in O(n) time.
+
+// Example 1:
+
+// Input: nums = [100,4,200,1,3,2]
+// Output: 4
+// Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4
+
+let longestSequence = (nums) => {
+    const numSet = new Set(nums);
+    let longestStreak = 0;
+
+    for (const num of numSet) {
+        if (!numSet.has(num - 1)) {
+            let currentNum = num;
+            let currentStreak = 1;
+
+            while (numSet.has(currentNum + 1)) {
+                currentNum += 1;
+                currentStreak += 1;
+            }
+
+            longestStreak = Math.max(longestStreak, currentStreak);
+        }
+    }
+
+    return longestStreak;
+};
+
+console.log(longestSequence([100, 4, 200, 1, 3, 2])); //4
